00a5e6a9f0b4ae260653948be5149225e0de255f
Boris Karabut <boriskarabytwork@gmail.com>
2024-03-24 18:28:56 +0300

    chore: добавил биомы для планет на карте

---
+++ data/planetBiomes/index.ts
@@ -0,0 +1,74 @@
+import { BiomesObject } from "@/utils/dataInterfaces";
+
+export const biomes: BiomesObject = {
+  rainForest: {
+    surface: "дождевой лес",
+    description:
+      "Странное изменение процесса фотосинтеза, позволяющее флоре необычной раскраски процветать на этой планете, ставит в тупик величайших экзобиологов Супер-Земли.",
+  },
+  ethereal: {
+    surface: "эфирная",
+    description:
+      "This world teems with ethereal, boundless, and peculiar plant life that spreads silent and uninterrupted across its entire surface.",
+  },
+  jungle: {
+    surface: "джунгли",
+    description:
+      "Эта богатая жизнью планета покрыта глубокими океанами, густыми лесами и роскошными лугами.",
+  },
+  moon: {
+    surface: "луна",
+    description:
+      "A rocky, lonely moon with extremely valuable mineral deposits underneath the surface.",
+  },
+  desert: {
+    surface: "пустыня",
+    description:
+      "Пустынная планета, где свирепствуют непредсказуемые и крайне опасные песчаные вихри.",
+  },
+  winter: {
+    surface: "зима",
+    description:
+      "Submerged in eternal winter, this world's frosty peaks glimmer in the light of its too-distant star.",
+  },
+  highlands: {
+    surface: "нагорье",
+    description:
+      "Каменистые образования пробиваются из высокой травы на этой туманной холмистой планете.",
+  },
+  iceMoss: {
+    surface: "ледяной мох",
+    description:
+      "Ice and moss-covered rock can be found across most of the surface of this planet.",
+  },
+  iceMossSpecial: {
+    surface: "специальный ледяной мох",
+    description:
+      "Ice and moss-covered rock can be found across most of the surface of this planet.",
+  },
+  tundra: {
+    surface: "тундра",
+    description:
+      "A perenially chilly climate has allowed short, colourful shrubs to flourish across this planet's surface.",
+  },
+  swamp: {
+    surface: "болото",
+    description:
+      "Безжизненная серость этой планеты нарушается лишь пятнами необычных фиолетовых цветов, произрастающими в виде странных паразитарных скоплений.",
+  },
+  desolate: {
+    surface: "пустынная",
+    description:
+      "Опаляющаяя жара, свирепые ветра и почти полное отсутствие осадков постоянно вызывают на планете пожары, прерываемые лишь короткими всплесками возрождения растительности.",
+  },
+  crimsonMoor: {
+    surface: "багровая луна",
+    description:
+      "Багряные дикорастущие водоросли покрывают всю планету, раскрашивая ее каменистые холмы глубоким красным. Этот цвет отлично маскирует пролитую кровь борцов с тиранией.",
+  },
+  canyon: {
+    surface: "каньон",
+    description:
+      "This arid, rocky biome covering this world has driven the evolution of exceptionally efficient water usage in its various organisms.",
+  },
+};
