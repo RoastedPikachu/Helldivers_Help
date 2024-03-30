624838e7e4dfb9e000216c672e75442c671db8ee
Boris Karabut <boriskarabytwork@gmail.com>
2024-03-30 18:10:33 +0300

    chore: Вынес бонусы брони и способы ее получения в отдельные файлы

---
+++ data/armorObtainingTypes/index.ts
@@ -0,0 +1,36 @@
+import { ArmorObtainingType } from "@/utils/dataInterfaces";
+
+export const obtainingType: { [key: number]: ArmorObtainingType } = {
+  0: {
+    typeNumber: 0,
+    name: "Обучение",
+  },
+  1: {
+    typeNumber: 1,
+    name: "Боевой пропуск «Адские Десантники, в бой!»",
+  },
+  2: {
+    typeNumber: 2,
+    name: "Боевой пропуск «Закаленные ветераны»",
+  },
+  3: {
+    typeNumber: 3,
+    name: "Боевой пропуск «На острие»",
+  },
+  4: {
+    typeNumber: 4,
+    name: "Супермагазин",
+  },
+  5: {
+    typeNumber: 5,
+    name: "Предзаказ",
+  },
+  6: {
+    typeNumber: 6,
+    name: "Super-Citizen издание",
+  },
+  7: {
+    typeNumber: 7,
+    name: "Twitch drops",
+  },
+};
