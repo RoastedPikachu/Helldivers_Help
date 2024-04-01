b61fd713d9b19ffd002086bcbee9c2a626ae7cc2
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-01 14:12:01 +0300

    chore: Поменял изображение и добавил тегов для твиттера

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,18 +12,26 @@ export const metadata: Metadata = {
     url: "https://helldivers.help",
     images: [
       {
-        url: "https://opengraph.b-cdn.net/production/documents/34ee463e-310b-4544-998d-0b632ac9e76b.png?token=taqbWFqaZ-I9IW7-QkB8RGd4EFSE6V_ggg7wlfQfSRo&height=630&width=1200&expires=33247965426",
+        url: "https://opengraph.b-cdn.net/production/documents/b6abc398-b62a-4ca3-be93-ed8964096447.png?token=Xx2BnhRVlWJOvfcF69nnqP-PeXQrmZ0KB-L7_XAoMSM&height=630&width=1200&expires=33247969668",
         width: 1200,
         height: 630,
       },
       {
-        url: "https://opengraph.b-cdn.net/production/documents/34ee463e-310b-4544-998d-0b632ac9e76b.png?token=taqbWFqaZ-I9IW7-QkB8RGd4EFSE6V_ggg7wlfQfSRo&height=1260&width=2400&expires=33247965426",
+        url: "https://opengraph.b-cdn.net/production/documents/b6abc398-b62a-4ca3-be93-ed8964096447.png?token=Xx2BnhRVlWJOvfcF69nnqP-PeXQrmZ0KB-L7_XAoMSM&height=1260&width=2400&expires=33247969668",
         width: 2400,
         height: 1260,
       },
     ],
     locale: "ru_RU",
     type: "website",
+  },
+  twitter: {
+    title: "Helldivers.help — Helldivers 2 помощник: Вики & другое",
+    description:
+      "Helldiver.help — это сайт-помощник каждому Адскому Десантнику, который сражается за продвижение демократии, свободы и процветания по всей галактике.",
+    images: {
+      url: "https://opengraph.b-cdn.net/production/documents/b6abc398-b62a-4ca3-be93-ed8964096447.png?token=Xx2BnhRVlWJOvfcF69nnqP-PeXQrmZ0KB-L7_XAoMSM&height=630&width=1200&expires=33247969668",
+    },
   },
 };
 
