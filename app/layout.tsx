56dc7775e0445044717760c5211dad4f2815d6cb
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-01 13:54:30 +0300

    fix: Попытка №5 добиться нормального отображения изображения

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,12 @@ export const metadata: Metadata = {
       {
         url: "https://opengraph.b-cdn.net/production/documents/34ee463e-310b-4544-998d-0b632ac9e76b.png?token=taqbWFqaZ-I9IW7-QkB8RGd4EFSE6V_ggg7wlfQfSRo&height=630&width=1200&expires=33247965426",
         width: 1200,
-        height: 600,
+        height: 630,
+      },
+      {
+        url: "https://opengraph.b-cdn.net/production/documents/34ee463e-310b-4544-998d-0b632ac9e76b.png?token=taqbWFqaZ-I9IW7-QkB8RGd4EFSE6V_ggg7wlfQfSRo&height=1260&width=2400&expires=33247965426",
+        width: 2400,
+        height: 1260,
       },
     ],
     locale: "ru_RU",
