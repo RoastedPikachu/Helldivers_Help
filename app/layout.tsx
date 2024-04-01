c97499024fc5c9e27ec56b78b4074c1178f61169
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-01 12:52:55 +0300

    chore: Добавил мета-тегов для open graph, а также изображение

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Helldivers.help: Сайт-помощник Адским Десантникам и борцам за демократию"
+        />
+        <meta
+          property="og:description"
+          content="Полный справочник по всем планетам, вооружению, снаряжению, противникам и многому другому, что может встретить на пути Адский Десантник"
+        />
+        <meta property="og:image" content="/static/OpenGraphImage.png" />
+        <meta property="og:url" content="https://helldivers.help" />
+      </Head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
