e461fe4a2410012579da187519073bbb6f3da22a
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-02 21:09:55 +0300

    chore: Добавил тип стратагемы

---
+++ data/stratagemTypes/index.ts
@@ -0,0 +1,32 @@
+import { StratagemType } from "@/utils/dataInterfaces";
+
+export const stratagemTypes: { [key: string]: StratagemType } = {
+  supportWeapon: {
+    name: "Оружие поддержки",
+    typeNumber: 0,
+  },
+  orbital: {
+    name: "Орбитальные пушки",
+    typeNumber: 1,
+  },
+  hangar: {
+    name: "Ангар",
+    typeNumber: 2,
+  },
+  bridge: {
+    name: "Капитанский мостик",
+    typeNumber: 3,
+  },
+  engineeringBay: {
+    name: "Инженерный отсек",
+    typeNumber: 4,
+  },
+  roboticsWorkshop: {
+    name: "Мастерская роботов",
+    typeNumber: 5,
+  },
+  mission: {
+    name: "Стратагемы миссий",
+    typeNumber: 6,
+  },
+};
