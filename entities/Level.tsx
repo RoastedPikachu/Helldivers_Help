2fb4d818d14fbab3e68aa316f91cdb97291a7359
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-04 15:25:25 +0300

    chore: Добавил заглушку для отсутствующих изображений рангов
    refactor: чуть-чуть изменил стили

--- entities/Level.tsx
+++ entities/Level.tsx
@@ -11,7 +11,7 @@ const Level: React.FC<LevelProps> = ({ id, xpRequired, xpTotal }) => {
   return (
     <div className="grid grid-rows-1 grid-cols-3 col-span-3 w-full h-full">
       <div
-        className={`flex justify-center items-center w-full h-[50px] bg-[#00293a] border-b-2 border-l-2 border-[--theme-color] ${id === 50 ? "rounded-bl-[10px]" : ""}`}
+        className={`flex justify-center items-center w-full h-[50px] bg-[#00293a] border-b-2 border-l-2 border-[--theme-color] ${id === 150 ? "rounded-bl-[10px]" : ""}`}
       >
         <p className="text-[#ffffff] text-[1.375rem] font-['Exo2'] font-medium brightness-75">
           {id}
@@ -25,7 +25,7 @@ const Level: React.FC<LevelProps> = ({ id, xpRequired, xpTotal }) => {
       </div>
 
       <div
-        className={`flex justify-center items-center w-full h-[50px] bg-[#00293a] border-b-2 border-r-2  border-[--theme-color] ${id === 50 ? "rounded-br-[10px]" : ""}`}
+        className={`flex justify-center items-center w-full h-[50px] bg-[#00293a] border-b-2 border-r-2  border-[--theme-color] ${id === 150 ? "rounded-br-[10px]" : ""}`}
       >
         <p className="text-[#ffffff] text-[1.375rem] font-['Exo2'] font-medium brightness-75">
           {xpTotal}
--- entities/Rank.tsx
+++ entities/Rank.tsx
@@ -11,7 +11,15 @@ interface RankProps {
 const Rank: React.FC<RankProps> = ({ iconPath, title, fromLevel, xpCount }) => {
   return (
     <div className="relative flex items-center px-[30px] pt-[20px] pb-[30px] w-full h-auto bg-[#00293a] border-2 border-[--theme-color] rounded-[10px]">
-      <img src={`${iconPath}`} alt="" className="w-[75px] h-[75px]" />
+      {iconPath ? (
+        <img src={`${iconPath}`} alt="" className="w-[75px] h-[75px]" />
+      ) : (
+        <div className="flex justify-center items-center w-[75px] h-[75px]">
+          <p className="text-[#2cc388] text-[4rem] font-['Insignia'] font-bold">
+            ?
+          </p>
+        </div>
+      )}
 
       <p className="ml-[20px] text-[#ffffff] text-[1.75rem] font-['Exo2'] font-bold">
         {title}
