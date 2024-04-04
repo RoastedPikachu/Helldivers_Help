f6a3c61e7d9efe79870c861f8cf5d7cd909b5901
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-04 15:24:26 +0300

    chore: Добавил новые уровни и ранги

--- data/levels/index.ts
+++ data/levels/index.ts
@@ -1,254 +1,754 @@
 import { Level } from "@/utils/generalInterfaces";
 
 export const levels = [
-    {
-        id: 1,
-        xpRequired: 0,
-        xpTotal: 0,
-    },
-    {
-        id: 2,
-        xpRequired: 100,
-        xpTotal: 100,
-    },
-    {
-        id: 3,
-        xpRequired: 250,
-        xpTotal: 350,
-    },
-    {
-        id: 4,
-        xpRequired: 500,
-        xpTotal: 850,
-    },
-    {
-        id: 5,
-        xpRequired: 750,
-        xpTotal: 1600,
-    },
-    {
-        id: 6,
-        xpRequired: 1000,
-        xpTotal: 2600,
-    },
-    {
-        id: 7,
-        xpRequired: 1000,
-        xpTotal: 3600,
-    },
-    {
-        id: 8,
-        xpRequired: 1000,
-        xpTotal: 4600,
-    },
-    {
-        id: 9,
-        xpRequired: 1250,
-        xpTotal: 5850,
-    },
-    {
-        id: 10,
-        xpRequired: 1250,
-        xpTotal: 7100,
-    },
-    {
-        id: 11,
-        xpRequired: 1300,
-        xpTotal: 8400,
-    },
-    {
-        id: 12,
-        xpRequired: 1500,
-        xpTotal: 9900,
-    },
-    {
-        id: 13,
-        xpRequired: 1500,
-        xpTotal: 11400,
-    },
-    {
-        id: 14,
-        xpRequired: 1500,
-        xpTotal: 12900,
-    },
-    {
-        id: 15,
-        xpRequired: 1500,
-        xpTotal: 14400,
-    },
-    {
-        id: 16,
-        xpRequired: 1500,
-        xpTotal: 15900,
-    },
-    {
-        id: 17,
-        xpRequired: 2000,
-        xpTotal: 17900,
-    },
-    {
-        id: 18,
-        xpRequired: 2000,
-        xpTotal: 19900,
-    },
-    {
-        id: 19,
-        xpRequired: 2000,
-        xpTotal: 21900,
-    },
-    {
-        id: 20,
-        xpRequired: 2000,
-        xpTotal: 23900,
-    },
-    {
-        id: 21,
-        xpRequired: 2000,
-        xpTotal: 25900,
-    },
-    {
-        id: 22,
-        xpRequired: 2500,
-        xpTotal: 28400,
-    },
-    {
-        id: 23,
-        xpRequired: 2500,
-        xpTotal: 30900,
-    },
-    {
-        id: 24,
-        xpRequired: 2500,
-        xpTotal: 33400,
-    },
-    {
-        id: 25,
-        xpRequired: 2500,
-        xpTotal: 35900,
-    },
-    {
-        id: 26,
-        xpRequired: 2500,
-        xpTotal: 38400,
-    },
-    {
-        id: 27,
-        xpRequired: 3000,
-        xpTotal: 41400,
-    },
-    {
-        id: 28,
-        xpRequired: 3000,
-        xpTotal: 44400,
-    },
-    {
-        id: 29,
-        xpRequired: 3000,
-        xpTotal: 47400,
-    },
-    {
-        id: 30,
-        xpRequired: 3000,
-        xpTotal: 50400,
-    },
-    {
-        id: 31,
-        xpRequired: 3000,
-        xpTotal: 53400,
-    },
-    {
-        id: 32,
-        xpRequired: 3500,
-        xpTotal: 56900,
-    },
-    {
-        id: 33,
-        xpRequired: 3500,
-        xpTotal: 60400,
-    },
-    {
-        id: 34,
-        xpRequired: 3500,
-        xpTotal: 63900,
-    },
-    {
-        id: 35,
-        xpRequired: 3500,
-        xpTotal: 67400,
-    },
-    {
-        id: 36,
-        xpRequired: 3500,
-        xpTotal: 70900,
-    },
-    {
-        id: 37,
-        xpRequired: 4000,
-        xpTotal: 74900,
-    },
-    {
-        id: 38,
-        xpRequired: 4000,
-        xpTotal: 78900,
-    },
-    {
-        id: 39,
-        xpRequired: 4000,
-        xpTotal: 82900,
-    },
-    {
-        id: 40,
-        xpRequired: 4000,
-        xpTotal: 86900,
-    },
-    {
-        id: 41,
-        xpRequired: 4000,
-        xpTotal: 90900,
-    },
-    {
-        id: 42,
-        xpRequired: 4500,
-        xpTotal: 95400,
-    },
-    {
-        id: 43,
-        xpRequired: 4500,
-        xpTotal: 99900,
-    },
-    {
-        id: 44,
-        xpRequired: 4500,
-        xpTotal: 104400,
-    },
-    {
-        id: 45,
-        xpRequired: 4500,
-        xpTotal: 108900,
-    },
-    {
-        id: 46,
-        xpRequired: 4500,
-        xpTotal: 113400,
-    },
-    {
-        id: 47,
-        xpRequired: 5000,
-        xpTotal: 118400,
-    },
-    {
-        id: 48,
-        xpRequired: 5000,
-        xpTotal: 123400,
-    },
-    {
-        id: 49,
-        xpRequired: 5000,
-        xpTotal: 128400,
-    },
-    {
-        id: 50,
-        xpRequired: 5000,
-        xpTotal: 133400,
-    },
+  {
+    id: 1,
+    xpRequired: 0,
+    xpTotal: 0,
+  },
+  {
+    id: 2,
+    xpRequired: 100,
+    xpTotal: 100,
+  },
+  {
+    id: 3,
+    xpRequired: 250,
+    xpTotal: 350,
+  },
+  {
+    id: 4,
+    xpRequired: 500,
+    xpTotal: 850,
+  },
+  {
+    id: 5,
+    xpRequired: 750,
+    xpTotal: 1600,
+  },
+  {
+    id: 6,
+    xpRequired: 1000,
+    xpTotal: 2600,
+  },
+  {
+    id: 7,
+    xpRequired: 1000,
+    xpTotal: 3600,
+  },
+  {
+    id: 8,
+    xpRequired: 1000,
+    xpTotal: 4600,
+  },
+  {
+    id: 9,
+    xpRequired: 1250,
+    xpTotal: 5850,
+  },
+  {
+    id: 10,
+    xpRequired: 1250,
+    xpTotal: 7100,
+  },
+  {
+    id: 11,
+    xpRequired: 1300,
+    xpTotal: 8400,
+  },
+  {
+    id: 12,
+    xpRequired: 1500,
+    xpTotal: 9900,
+  },
+  {
+    id: 13,
+    xpRequired: 1500,
+    xpTotal: 11400,
+  },
+  {
+    id: 14,
+    xpRequired: 1500,
+    xpTotal: 12900,
+  },
+  {
+    id: 15,
+    xpRequired: 1500,
+    xpTotal: 14400,
+  },
+  {
+    id: 16,
+    xpRequired: 1500,
+    xpTotal: 15900,
+  },
+  {
+    id: 17,
+    xpRequired: 2000,
+    xpTotal: 17900,
+  },
+  {
+    id: 18,
+    xpRequired: 2000,
+    xpTotal: 19900,
+  },
+  {
+    id: 19,
+    xpRequired: 2000,
+    xpTotal: 21900,
+  },
+  {
+    id: 20,
+    xpRequired: 2000,
+    xpTotal: 23900,
+  },
+  {
+    id: 21,
+    xpRequired: 2000,
+    xpTotal: 25900,
+  },
+  {
+    id: 22,
+    xpRequired: 2500,
+    xpTotal: 28400,
+  },
+  {
+    id: 23,
+    xpRequired: 2500,
+    xpTotal: 30900,
+  },
+  {
+    id: 24,
+    xpRequired: 2500,
+    xpTotal: 33400,
+  },
+  {
+    id: 25,
+    xpRequired: 2500,
+    xpTotal: 35900,
+  },
+  {
+    id: 26,
+    xpRequired: 2500,
+    xpTotal: 38400,
+  },
+  {
+    id: 27,
+    xpRequired: 3000,
+    xpTotal: 41400,
+  },
+  {
+    id: 28,
+    xpRequired: 3000,
+    xpTotal: 44400,
+  },
+  {
+    id: 29,
+    xpRequired: 3000,
+    xpTotal: 47400,
+  },
+  {
+    id: 30,
+    xpRequired: 3000,
+    xpTotal: 50400,
+  },
+  {
+    id: 31,
+    xpRequired: 3000,
+    xpTotal: 53400,
+  },
+  {
+    id: 32,
+    xpRequired: 3500,
+    xpTotal: 56900,
+  },
+  {
+    id: 33,
+    xpRequired: 3500,
+    xpTotal: 60400,
+  },
+  {
+    id: 34,
+    xpRequired: 3500,
+    xpTotal: 63900,
+  },
+  {
+    id: 35,
+    xpRequired: 3500,
+    xpTotal: 67400,
+  },
+  {
+    id: 36,
+    xpRequired: 3500,
+    xpTotal: 70900,
+  },
+  {
+    id: 37,
+    xpRequired: 4000,
+    xpTotal: 74900,
+  },
+  {
+    id: 38,
+    xpRequired: 4000,
+    xpTotal: 78900,
+  },
+  {
+    id: 39,
+    xpRequired: 4000,
+    xpTotal: 82900,
+  },
+  {
+    id: 40,
+    xpRequired: 4000,
+    xpTotal: 86900,
+  },
+  {
+    id: 41,
+    xpRequired: 4000,
+    xpTotal: 90900,
+  },
+  {
+    id: 42,
+    xpRequired: 4500,
+    xpTotal: 95400,
+  },
+  {
+    id: 43,
+    xpRequired: 4500,
+    xpTotal: 99900,
+  },
+  {
+    id: 44,
+    xpRequired: 4500,
+    xpTotal: 104400,
+  },
+  {
+    id: 45,
+    xpRequired: 4500,
+    xpTotal: 108900,
+  },
+  {
+    id: 46,
+    xpRequired: 4500,
+    xpTotal: 113400,
+  },
+  {
+    id: 47,
+    xpRequired: 5000,
+    xpTotal: 118400,
+  },
+  {
+    id: 48,
+    xpRequired: 5000,
+    xpTotal: 123400,
+  },
+  {
+    id: 49,
+    xpRequired: 5000,
+    xpTotal: 128400,
+  },
+  {
+    id: 50,
+    xpRequired: 5000,
+    xpTotal: 133400,
+  },
+  {
+    id: 51,
+    xpRequired: 5000,
+    xpTotal: 138400,
+  },
+  {
+    id: 52,
+    xpRequired: 5500,
+    xpTotal: 143900,
+  },
+  {
+    id: 53,
+    xpRequired: 5500,
+    xpTotal: 149400,
+  },
+  {
+    id: 54,
+    xpRequired: 5500,
+    xpTotal: 154900,
+  },
+  {
+    id: 55,
+    xpRequired: 5500,
+    xpTotal: 160400,
+  },
+  {
+    id: 56,
+    xpRequired: 5500,
+    xpTotal: 165900,
+  },
+  {
+    id: 57,
+    xpRequired: 6000,
+    xpTotal: 171900,
+  },
+  {
+    id: 58,
+    xpRequired: 6000,
+    xpTotal: 177900,
+  },
+  {
+    id: 59,
+    xpRequired: 6000,
+    xpTotal: 183900,
+  },
+  {
+    id: 60,
+    xpRequired: 6000,
+    xpTotal: 189900,
+  },
+  {
+    id: 61,
+    xpRequired: 6000,
+    xpTotal: 195900,
+  },
+  {
+    id: 62,
+    xpRequired: 6500,
+    xpTotal: 202400,
+  },
+  {
+    id: 63,
+    xpRequired: 6500,
+    xpTotal: 208900,
+  },
+  {
+    id: 64,
+    xpRequired: 6500,
+    xpTotal: 215400,
+  },
+  {
+    id: 65,
+    xpRequired: 6500,
+    xpTotal: 221900,
+  },
+  {
+    id: 66,
+    xpRequired: 6500,
+    xpTotal: 228400,
+  },
+  {
+    id: 67,
+    xpRequired: 7000,
+    xpTotal: 235400,
+  },
+  {
+    id: 68,
+    xpRequired: 7000,
+    xpTotal: 242400,
+  },
+  {
+    id: 69,
+    xpRequired: 7000,
+    xpTotal: 249400,
+  },
+  {
+    id: 70,
+    xpRequired: 7000,
+    xpTotal: 256400,
+  },
+  {
+    id: 71,
+    xpRequired: 7000,
+    xpTotal: 263400,
+  },
+  {
+    id: 72,
+    xpRequired: 7500,
+    xpTotal: 270900,
+  },
+  {
+    id: 73,
+    xpRequired: 7500,
+    xpTotal: 278400,
+  },
+  {
+    id: 74,
+    xpRequired: 7500,
+    xpTotal: 285900,
+  },
+  {
+    id: 75,
+    xpRequired: 7500,
+    xpTotal: 293400,
+  },
+  {
+    id: 76,
+    xpRequired: 7500,
+    xpTotal: 300900,
+  },
+  {
+    id: 77,
+    xpRequired: 8000,
+    xpTotal: 308900,
+  },
+  {
+    id: 78,
+    xpRequired: 8000,
+    xpTotal: 316900,
+  },
+  {
+    id: 79,
+    xpRequired: 8000,
+    xpTotal: 324900,
+  },
+  {
+    id: 80,
+    xpRequired: 8000,
+    xpTotal: 332900,
+  },
+  {
+    id: 81,
+    xpRequired: 8000,
+    xpTotal: 340900,
+  },
+  {
+    id: 82,
+    xpRequired: 8500,
+    xpTotal: 349400,
+  },
+  {
+    id: 83,
+    xpRequired: 8500,
+    xpTotal: 357900,
+  },
+  {
+    id: 84,
+    xpRequired: 8500,
+    xpTotal: 366400,
+  },
+  {
+    id: 85,
+    xpRequired: 8500,
+    xpTotal: 374900,
+  },
+  {
+    id: 86,
+    xpRequired: 8500,
+    xpTotal: 383400,
+  },
+  {
+    id: 87,
+    xpRequired: 9000,
+    xpTotal: 392400,
+  },
+  {
+    id: 88,
+    xpRequired: 9000,
+    xpTotal: 401400,
+  },
+  {
+    id: 89,
+    xpRequired: 9000,
+    xpTotal: 410400,
+  },
+  {
+    id: 90,
+    xpRequired: 9000,
+    xpTotal: 419400,
+  },
+  {
+    id: 91,
+    xpRequired: 9000,
+    xpTotal: 428400,
+  },
+  {
+    id: 92,
+    xpRequired: 9500,
+    xpTotal: 437900,
+  },
+  {
+    id: 93,
+    xpRequired: 9500,
+    xpTotal: 447400,
+  },
+  {
+    id: 94,
+    xpRequired: 9500,
+    xpTotal: 456900,
+  },
+  {
+    id: 95,
+    xpRequired: 9500,
+    xpTotal: 466400,
+  },
+  {
+    id: 96,
+    xpRequired: 9500,
+    xpTotal: 475900,
+  },
+  {
+    id: 97,
+    xpRequired: 10000,
+    xpTotal: 485900,
+  },
+  {
+    id: 98,
+    xpRequired: 10000,
+    xpTotal: 495900,
+  },
+  {
+    id: 99,
+    xpRequired: 10000,
+    xpTotal: 505900,
+  },
+  {
+    id: 100,
+    xpRequired: 10000,
+    xpTotal: 515900,
+  },
+  {
+    id: 101,
+    xpRequired: 10000,
+    xpTotal: 525900,
+  },
+  {
+    id: 102,
+    xpRequired: 10500,
+    xpTotal: 536400,
+  },
+  {
+    id: 103,
+    xpRequired: 10500,
+    xpTotal: 546900,
+  },
+  {
+    id: 104,
+    xpRequired: 10500,
+    xpTotal: 557400,
+  },
+  {
+    id: 105,
+    xpRequired: 10500,
+    xpTotal: 567900,
+  },
+  {
+    id: 106,
+    xpRequired: 10500,
+    xpTotal: 578400,
+  },
+  {
+    id: 107,
+    xpRequired: 11000,
+    xpTotal: 589400,
+  },
+  {
+    id: 108,
+    xpRequired: 11000,
+    xpTotal: 600400,
+  },
+  {
+    id: 109,
+    xpRequired: 11000,
+    xpTotal: 611400,
+  },
+  {
+    id: 110,
+    xpRequired: 11000,
+    xpTotal: 622400,
+  },
+  {
+    id: 111,
+    xpRequired: 11000,
+    xpTotal: 633400,
+  },
+  {
+    id: 112,
+    xpRequired: 11500,
+    xpTotal: 644900,
+  },
+  {
+    id: 113,
+    xpRequired: 11500,
+    xpTotal: 656400,
+  },
+  {
+    id: 114,
+    xpRequired: 11500,
+    xpTotal: 667900,
+  },
+  {
+    id: 115,
+    xpRequired: 11500,
+    xpTotal: 679400,
+  },
+  {
+    id: 116,
+    xpRequired: 11500,
+    xpTotal: 690900,
+  },
+  {
+    id: 117,
+    xpRequired: 12000,
+    xpTotal: 702900,
+  },
+  {
+    id: 118,
+    xpRequired: 12000,
+    xpTotal: 714900,
+  },
+  {
+    id: 119,
+    xpRequired: 12000,
+    xpTotal: 726900,
+  },
+  {
+    id: 120,
+    xpRequired: 12000,
+    xpTotal: 738900,
+  },
+  {
+    id: 121,
+    xpRequired: 12000,
+    xpTotal: 750900,
+  },
+  {
+    id: 122,
+    xpRequired: 12500,
+    xpTotal: 763400,
+  },
+  {
+    id: 123,
+    xpRequired: 12500,
+    xpTotal: 775900,
+  },
+  {
+    id: 124,
+    xpRequired: 12500,
+    xpTotal: 788400,
+  },
+  {
+    id: 125,
+    xpRequired: 12500,
+    xpTotal: 800900,
+  },
+  {
+    id: 126,
+    xpRequired: 12500,
+    xpTotal: 813400,
+  },
+  {
+    id: 127,
+    xpRequired: 13000,
+    xpTotal: 826400,
+  },
+  {
+    id: 128,
+    xpRequired: 13000,
+    xpTotal: 839400,
+  },
+  {
+    id: 129,
+    xpRequired: 13000,
+    xpTotal: 852400,
+  },
+  {
+    id: 130,
+    xpRequired: 13000,
+    xpTotal: 865400,
+  },
+  {
+    id: 131,
+    xpRequired: 13000,
+    xpTotal: 878400,
+  },
+  {
+    id: 132,
+    xpRequired: 13500,
+    xpTotal: 891900,
+  },
+  {
+    id: 133,
+    xpRequired: 13500,
+    xpTotal: 905400,
+  },
+  {
+    id: 134,
+    xpRequired: 13500,
+    xpTotal: 918900,
+  },
+  {
+    id: 135,
+    xpRequired: 13500,
+    xpTotal: 932400,
+  },
+  {
+    id: 136,
+    xpRequired: 13500,
+    xpTotal: 945900,
+  },
+  {
+    id: 137,
+    xpRequired: 14000,
+    xpTotal: 959900,
+  },
+  {
+    id: 138,
+    xpRequired: 14000,
+    xpTotal: 973900,
+  },
+  {
+    id: 139,
+    xpRequired: 14000,
+    xpTotal: 987900,
+  },
+  {
+    id: 140,
+    xpRequired: 14000,
+    xpTotal: 1001900,
+  },
+  {
+    id: 141,
+    xpRequired: 14000,
+    xpTotal: 1015900,
+  },
+  {
+    id: 142,
+    xpRequired: 14500,
+    xpTotal: 1030400,
+  },
+  {
+    id: 143,
+    xpRequired: 14500,
+    xpTotal: 1044900,
+  },
+  {
+    id: 144,
+    xpRequired: 14500,
+    xpTotal: 1059400,
+  },
+  {
+    id: 145,
+    xpRequired: 14500,
+    xpTotal: 1073900,
+  },
+  {
+    id: 146,
+    xpRequired: 14500,
+    xpTotal: 1088400,
+  },
+  {
+    id: 147,
+    xpRequired: 15000,
+    xpTotal: 1103400,
+  },
+  {
+    id: 148,
+    xpRequired: 15000,
+    xpTotal: 1118400,
+  },
+  {
+    id: 149,
+    xpRequired: 15000,
+    xpTotal: 1133400,
+  },
+  {
+    id: 150,
+    xpRequired: 15000,
+    xpTotal: 1148400,
+  },
 ] as Level[];
