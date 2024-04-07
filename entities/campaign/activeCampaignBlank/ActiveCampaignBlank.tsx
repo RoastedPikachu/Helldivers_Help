0bd4e1e58d0f7d5903a5576aa4bbb2c20b88f068
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-07 17:53:22 +0300

    feat: Добавил компонент-заглушку для текущих сражений
    chore: Также объединил компоненты сражений в одну директорию

---
+++ entities/campaign/activeCampaignBlank/ActiveCampaignBlank.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./ActiveCampaignBlank.css";
+
+const ActiveCampaignBlank = () => {
+  return (
+    <div>
+      <p className="blankText"></p>
+
+      <div className="blankBlock "></div>
+    </div>
+  );
+};
+
+export default ActiveCampaignBlank;
