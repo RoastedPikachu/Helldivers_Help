61a67a2e96654b530b918d35aa47a38e86c3ae91
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-08 12:10:00 +0300

    feat: Добавил компонент заглушки для главного приказа

---
+++ entities/orders/MajorOrderBlank/MajorOrderBlank.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./MajorOrderBlank.css";
+
+const MajorOrderBlank = () => {
+  return <div className="rootMajorOrderBlankWidget"></div>;
+};
+
+export default MajorOrderBlank;
