cc0144cf711c78326898af7c2d335cfa2f522f7d
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-08 12:10:44 +0300

    feat: Добавил компонент отсутствующего главного приказа

---
+++ entities/orders/noMajorOrder/NoMajorOrder.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./NoMajorOrder.css";
+
+const NoMajorOrder = () => {
+  return (
+    <div className="rootNoMajorOrderWidget">
+      <h3 className="rootNoMajorOrderWidget_Text">
+        На текущий момент приказов нет, отдыхай, солдат.
+      </h3>
+    </div>
+  );
+};
+
+export default NoMajorOrder;
