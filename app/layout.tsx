482012d4ef13402f41393f3d42567041bbde8bd2
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-09 13:22:38 +0300

    feat: Добавил яндекс метрику

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -44,7 +45,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script src="/static/metric/index.js" strategy="lazyOnload" />
+
+        {children}
+      </body>
     </html>
   );
 }
