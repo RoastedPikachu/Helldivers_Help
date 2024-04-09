dc7240a170fa142ed89ea549542f1ea1e333b7bd
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-09 23:34:31 +0300

    feat: Внедрил слайдер на страницу терминидов

--- app/terminids/page.tsx
+++ app/terminids/page.tsx
@@ -1,7 +1,12 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 import { observer, Observer } from "mobx-react-lite";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import { fractions } from "@/data/fractions";
 
 import { enemyTypeStore } from "@/store/EnemyTypeStore";
 
@@ -13,10 +18,34 @@ import TheScrollToUpButton from "@/shared/TheScrollToUpButton/TheScrollToUpButto
 import ThePageTitle from "@/shared/ThePageTitle";
 import PageDescription from "@/shared/PageDescription";
 
-import EnemyType from "@/entities/enemyType/EnemyType";
-import CurrentEnemyType from "@/entities/CurrentEnemyType";
+import EnemyType from "@/entities/enemiesPage/enemyType/EnemyType";
+import EnemyTypeAdditionalInfoModalWindow from "@/entities/enemiesPage/enemyTypeAdditionalInfoModalWindow/EnemyTypeAdditionalInfoModalWindow";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "./Terminids.css";
 
 const Page = observer(() => {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+  const [swiper, setSwiper] = useState(null as any);
+
+  const handleCurrentSlideChange = (id: number) => {
+    swiper.slideTo(id - 1);
+  };
+
+  const handleSlideChange = (swiper: any) => {
+    setCurrentSlideIndex(swiper?.activeIndex);
+  };
+
+  const getTargetEnemyTypeArray = () => {
+    if (enemyTypeStore.currentEnemyType.fraction === fractions["2"]) {
+      return enemyTypeStore.terminids;
+    }
+
+    return enemyTypeStore.automatons;
+  };
   return (
     <Observer>
       {() => (
@@ -48,22 +77,54 @@ const Page = observer(() => {
               }
             />
 
-            <section className="relative grid grid-cols-3 grid-rows-2 gap-y-[30px] gap-x-[30px] mt-[50px] w-full h-auto">
+            <section className="pageSection">
+              <div
+                className={`pageSection_Block_SliderWrapper ${enemyTypeStore.currentEnemyType.id ? "flex" : "hidden"}`}
+              >
+                <div className="pageSection_Block_SliderWrapper_DarkBackground"></div>
+
+                <Swiper
+                  spaceBetween={150}
+                  slidesPerView={1}
+                  loop={true}
+                  centeredSlides={false}
+                  modules={[Navigation]}
+                  navigation={true}
+                  onSwiper={(swiper) => setSwiper(swiper)}
+                  onSlideChange={handleSlideChange}
+                  className="pageSection_Block_SliderWrapper_Slider"
+                >
+                  {getTargetEnemyTypeArray()?.map((enemy) => (
+                    <SwiperSlide
+                      key={enemy.id}
+                      className="pageSection_Block_SliderWrapper_Slider_Slide"
+                    >
+                      <EnemyTypeAdditionalInfoModalWindow
+                        id={enemy.id}
+                        fraction={enemy.fraction}
+                        iconPath={enemy.iconPath}
+                        title={enemy.title}
+                        dangerous={enemy.dangerous}
+                        tactics={enemy.tactics}
+                        difficulties={enemy.difficulties}
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
+
               {enemyTypeStore.terminids.map((terminid) => (
                 <EnemyType
                   key={terminid.id}
                   id={terminid.id}
                   iconPath={terminid.iconPath}
                   rootBlockStyles={"w-[300px] h-[170px]"}
-                  fractionType={1}
+                  fraction={fractions["2"]}
                   imagePlugStyles={"mt-[15px] text-[6rem]"}
                   title={terminid.title}
+                  handleCurrentSlideChange={handleCurrentSlideChange}
                 />
               ))}
-
-              {enemyTypeStore.currentEnemyType.id && (
-                <CurrentEnemyType rootBlockStyles={"w-[400px] h-[230px]"} />
-              )}
             </section>
           </main>
 
