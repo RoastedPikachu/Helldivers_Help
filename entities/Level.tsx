1e3b9ec98c66cbc71d681188c77cf6c723bc478c
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-15 19:36:07 +0300

    chore: Небольшие изменения стилей

--- entities/Level.tsx
+++ entities/Level.tsx
@@ -13,13 +13,13 @@ const Level: React.FC<LevelProps> = ({ id, xpRequired, xpTotal }) => {
       <div
         className={`flex justify-center items-center w-full h-[50px] bg-[#00293a] border-b-2 border-l-2 border-[--theme-color] ${id === 150 ? "rounded-bl-[10px]" : ""}`}
       >
-        <p className="text-[#ffffff] text-[1.375rem] font-['Exo2'] font-medium brightness-75">
+        <p className="text-[#ffffff] text-[1.375rem] font-['Insignia'] font-medium brightness-75">
           {id}
         </p>
       </div>
 
       <div className="flex justify-center items-center w-full h-[50px] bg-[#00293a] border-b-2 border-x-2 border-[--theme-color]">
-        <p className="text-[#ffffff] text-[1.375rem] font-['Exo2'] font-medium brightness-75">
+        <p className="text-[#ffffff] text-[1.375rem] font-['Insignia'] font-medium brightness-75">
           {xpRequired}
         </p>
       </div>
@@ -27,7 +27,7 @@ const Level: React.FC<LevelProps> = ({ id, xpRequired, xpTotal }) => {
       <div
         className={`flex justify-center items-center w-full h-[50px] bg-[#00293a] border-b-2 border-r-2  border-[--theme-color] ${id === 150 ? "rounded-br-[10px]" : ""}`}
       >
-        <p className="text-[#ffffff] text-[1.375rem] font-['Exo2'] font-medium brightness-75">
+        <p className="text-[#ffffff] text-[1.375rem] font-['Insignia'] font-medium brightness-75">
           {xpTotal}
         </p>
       </div>
