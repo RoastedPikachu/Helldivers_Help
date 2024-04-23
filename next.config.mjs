9b42ac7e890166117ec2b9808312d537dfd54909
Boris Karabut <boriskarabytwork@gmail.com>
2024-04-23 11:39:05 +0300

    fix: Попытка пофиксить неотображающиеся изображения

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    images: {
+        unoptimized: true,
+        domains: [
+            "helldivers.help",
+            "helldivers-help.vercel.app",
+        ]
+    },
 };
 
 export default nextConfig;
