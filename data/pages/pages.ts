3368687b8c2a514616c5807dfed727039353ac20
Boris Karabut <boriskarabytwork@gmail.com>
2024-05-02 13:04:09 +0300

    refactor: Вынес список страниц в отдельный файл

---
+++ data/pages/pages.ts
@@ -0,0 +1,58 @@
+export const pages = [
+  { id: 1, link: "/weaponry", title: "Вооружение" },
+  {
+    id: 2,
+    link: "/armorKits",
+    title: "Броня",
+  },
+  {
+    id: 3,
+    link: "/stratagems",
+    title: "Стратагемы",
+  },
+  {
+    id: 4,
+    link: "/boosters",
+    title: "Усилители",
+  },
+  {
+    id: 5,
+    link: "/resources",
+    title: "Ресурсы",
+  },
+  {
+    id: 6,
+    link: "/shipModules",
+    title: "Модули корабля",
+  },
+  {
+    id: 7,
+    link: "/progression",
+    title: "Прогрессия",
+  },
+  {
+    id: 8,
+    link: "/achievements",
+    title: "Достижения",
+  },
+  {
+    id: 9,
+    link: "/enemy/terminids",
+    title: "Терминиды",
+  },
+  {
+    id: 10,
+    link: "/enemy/automatons",
+    title: "Автоматоны",
+  },
+  {
+    id: 11,
+    link: "/missions",
+    title: "Миссии",
+  },
+  {
+    id: 12,
+    link: "/planets",
+    title: "Планеты",
+  },
+];
