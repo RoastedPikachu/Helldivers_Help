180ebb2535b44195503755a716ac20f8c1350308
Boris Karabut <boriskarabytwork@gmail.com>
2024-05-06 15:17:17 +0300

    fix: Пофиксил прилипшую стрелочку

--- app/enemy/[fractionName]/[enemyName]/page.tsx
+++ app/enemy/[fractionName]/[enemyName]/page.tsx
@@ -28,7 +28,7 @@ const Page: React.FC<{ params: any }> = ({ params }) => {
 
       <RunningLine />
 
-      <main className="grid mlarge:block grid-cols-3 mlarge:!mt-[70px]">
+      <main className="grid mlarge:block grid-cols-3 mlarge:!mt-[20px]">
         <EnemyAdditionalInfoContent />
       </main>
 
