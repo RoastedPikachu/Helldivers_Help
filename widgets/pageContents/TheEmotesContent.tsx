57f1c4df2166f960360e267de8fa3604cd3549ea
Boris Karabut <boriskarabytwork@gmail.com>
2024-06-11 22:05:39 +0300

    feat: Добавил страницу с эмоциями и победными позами

---
+++ widgets/pageContents/TheEmotesContent.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { emotes } from "@/data/emotes";
+
+import LegendSection from "@/widgets/legendSection/LegendSection";
+import SearchBar from "@/widgets/searchBar/SearchBar";
+import EntitySection from "@/widgets/EntitySection";
+
+import ThePageTitle from "@/shared/ThePageTitle";
+import PageDescription from "@/shared/PageDescription";
+
+import Emote from "@/entities/emote/Emote";
+
+const TheEmotesContent = () => {
+  return (
+    <main>
+      <ThePageTitle title={"эмоции"} additionalTitle={"адского десантника"} />
+
+      <PageDescription
+        description={
+          "Во время боев любой Адский Десантник накапливает определенную долю впечатлений или переживаний. Их можно легко выплеснуть в эмоциях! А победу и успешное возвращение отпраздновать красивой победной позой. Покажите всем свои мускулы или обнимите любимого товарища, главное сохраняйте рассудок для дальнейшего продолжения распространения демократии по всей галактике."
+        }
+      />
+
+      <LegendSection />
+
+      <SearchBar />
+
+      <EntitySection
+        title={""}
+        gridStyles={"mt-[50px] mlarge:mt-[30px] grid-cols-6 mlarge:grid-cols-2"}
+      >
+        {emotes.map((emote) => (
+          <Emote
+            key={emote.id}
+            name={emote.name}
+            videoPreviewPath={emote.videoPreviewPath}
+            videoPath={emote.videoPath}
+            price={emote.price}
+            warbondIcon={emote.warbondIcon}
+          />
+        ))}
+      </EntitySection>
+    </main>
+  );
+};
+
+export default TheEmotesContent;
