988c56aa628ca3cbeb72abca6dba847599d7b9bc
Boris Karabut <boriskarabytwork@gmail.com>
2024-06-20 23:44:26 +0300

    feat: Добавил победные позы на страницу эмоций

--- widgets/pageContents/TheEmotesContent.tsx
+++ widgets/pageContents/TheEmotesContent.tsx
@@ -10,6 +10,7 @@ import ThePageTitle from "@/shared/ThePageTitle";
 import PageDescription from "@/shared/PageDescription";
 
 import Emote from "@/entities/emote/Emote";
+import { victoryPoses } from "@/data/victoryPoses";
 
 const TheEmotesContent = () => {
   return (
@@ -38,6 +39,22 @@ const TheEmotesContent = () => {
             videoPath={emote.videoPath}
             price={emote.price}
             warbondIcon={emote.warbondIcon}
+          />
+        ))}
+      </EntitySection>
+
+      <EntitySection
+        title={"ПОБЕДНЫЕ ПОЗЫ"}
+        gridStyles={"mt-[50px] mlarge:mt-[30px] grid-cols-6 mlarge:grid-cols-2"}
+      >
+        {victoryPoses.map((victoryPose) => (
+          <Emote
+            key={victoryPose.id}
+            name={victoryPose.name}
+            videoPreviewPath={victoryPose.videoPreviewPath}
+            videoPath={victoryPose.videoPath}
+            price={victoryPose.price}
+            warbondIcon={victoryPose.warbondIcon}
           />
         ))}
       </EntitySection>
