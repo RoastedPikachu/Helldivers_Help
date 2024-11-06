75345b6dd576d37de45b220299d8eeccc56d6689
Boris Karabut <boriskarabytwork@gmail.com>
2024-11-06 16:04:01 +0300

    chore: заменил фавиконку

--- data/warbonds/index.ts
+++ data/warbonds/index.ts
@@ -7,42 +7,42 @@ interface Warbond {
 export const warbonds: Warbond[] = [
   {
     id: 1,
-    image: "/static/warbonds/HelldiversMobilize.png",
+    image: "/static/warbonds/HelldiversMobilize.webp",
     title: "",
   },
   {
     id: 2,
-    image: "/static/warbonds/SteeledVeterans.png",
+    image: "/static/warbonds/SteeledVeterans.webp",
     title: "",
   },
   {
     id: 3,
-    image: "/static/warbonds/CuttingEdge.png",
+    image: "/static/warbonds/CuttingEdge.webp",
     title: "",
   },
   {
     id: 4,
-    image: "/static/warbonds/DemocraticDetonation.png",
+    image: "/static/warbonds/DemocraticDetonation.webp",
     title: "",
   },
   {
     id: 5,
-    image: "/static/warbonds/PolarPatriots.png",
+    image: "/static/warbonds/PolarPatriots.webp",
     title: "",
   },
   {
     id: 6,
-    image: "/static/warbonds/FreedomsFlame.png",
+    image: "/static/warbonds/FreedomsFlame.webp",
     title: "",
   },
   {
     id: 7,
-    image: "/static/warbonds/ChemicalAgents.png",
+    image: "/static/warbonds/ChemicalAgents.webp",
     title: "",
   },
   {
     id: 8,
-    image: "/static/warbonds/TruthEnforcers.png",
+    image: "/static/warbonds/TruthEnforcers.webp",
     title: "",
   },
 ];
--- widgets/pageContents/warbondsContent/TheWarbondsContent.tsx
+++ widgets/pageContents/warbondsContent/TheWarbondsContent.tsx
@@ -14,13 +14,18 @@ const TheWarbondsContent = () => {
         gridStyles={"mt-[50px] mlarge:mt-[30px] grid-cols-1"}
       >
         {warbonds.map((warbond) => (
-          <Link
-            key={warbond.id}
-            href={""}
-            className="mx-auto w-[1000px] h-[500px] border-2 border-[#95939e] hover:border-[#ffe500] hover:brightness-110 duration-300 ease-in-out"
-          >
-            <img src={warbond.image} alt="" className="w-full h-full" />
-          </Link>
+          <div key={warbond.id} className="w-full h-auto">
+            <Link
+              href={""}
+              className="block mx-auto w-[1000px] h-[500px] border-2 border-[#95939e] hover:border-[#ffe500] hover:brightness-110 duration-300 ease-in-out"
+            >
+              <img src={warbond.image} alt="" className="w-full h-full" />
+            </Link>
+
+            <p className="mt-[15px] mx-auto text-[--color-theme] text-[1.25rem] text-center font-primary">
+              {warbond.title}
+            </p>
+          </div>
         ))}
       </EntitySection>
     </main>
