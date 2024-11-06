d12926c7148591d34bfa58893124b29e9b48a306
Boris Karabut <boriskarabytwork@gmail.com>
2024-11-07 00:15:34 +0300

    feat: добавил шаблон для второго варбонда 'на острие'

--- widgets/pageContents/TheWarbondContent.tsx
+++ widgets/pageContents/TheWarbondContent.tsx
@@ -11,11 +11,95 @@ const TheWarbondContent = () => {
         title={""}
         gridStyles={"mt-[50px] mlarge:mt-[30px] grid-cols-1"}
       >
+        {/*<div className="flex items-center gap-x-[17px] mx-auto w-[1000px] h-[540px]">*/}
+        {/*  <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[401px] h-full">*/}
+        {/*    <WarbondItem width={192} height={351} price={2} />*/}
+
+        {/*    <WarbondItem width={192} height={351} price={15} />*/}
+
+        {/*    <WarbondItem width={401} height={172} price={8} />*/}
+        {/*  </div>*/}
+
+        {/*  <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[610px] h-full">*/}
+        {/*    <div className="flex gap-x-[17px] w-full">*/}
+        {/*      <WarbondItem width={192} height={172} price={7} />*/}
+
+        {/*      <WarbondItem width={401} height={172} price={20} />*/}
+        {/*    </div>*/}
+
+        {/*    <WarbondItem width={401} height={351} price={18} />*/}
+
+        {/*    <div className="grid gap-y-[17px] h-auto">*/}
+        {/*      <WarbondItem width={192} height={167} price={12} />*/}
+
+        {/*      <WarbondItem width={192} height={167} price={8} />*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+
+        {/*<div className="flex items-center gap-x-[17px] mx-auto w-[1000px] h-[540px]">*/}
+        {/*  <div className="flex gap-x-[17px] gap-y-[17px] w-[401px] h-full">*/}
+        {/*    <div className="grid gap-y-[17px] w-[192px] h-full">*/}
+        {/*      <WarbondItem width={192} height={172} price={15} />*/}
+
+        {/*      <WarbondItem width={192} height={351} price={5} />*/}
+        {/*    </div>*/}
+
+        {/*    <div className="grid gap-y-[17px] w-[192px] h-full">*/}
+        {/*      <WarbondItem width={192} height={351} price={25} />*/}
+
+        {/*      <WarbondItem width={192} height={172} price={16} />*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+
+        {/*  <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[610px] h-full">*/}
+        {/*    <WarbondItem width={401} height={351} price={45} />*/}
+
+        {/*    <div className="grid gap-y-[17px] h-auto">*/}
+        {/*      <WarbondItem width={192} height={167} price={35} />*/}
+
+        {/*      <WarbondItem width={192} height={167} price={16} />*/}
+        {/*    </div>*/}
+
+        {/*    <div className="flex gap-x-[17px] w-full">*/}
+        {/*      <WarbondItem width={192} height={172} price={12} />*/}
+
+        {/*      <WarbondItem width={401} height={172} price={60} />*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+
+        {/*<div className="flex items-center gap-x-[17px] mx-auto w-[1000px] h-[540px]">*/}
+        {/*  <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[401px] h-full">*/}
+        {/*    <WarbondItem width={192} height={351} price={16} />*/}
+
+        {/*    <WarbondItem width={192} height={351} price={75} />*/}
+
+        {/*    <WarbondItem width={401} height={172} price={32} />*/}
+        {/*  </div>*/}
+
+        {/*  <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[610px] h-full">*/}
+        {/*    <div className="flex gap-x-[17px] w-full">*/}
+        {/*      <WarbondItem width={192} height={172} price={32} />*/}
+
+        {/*      <WarbondItem width={401} height={172} price={80} />*/}
+        {/*    </div>*/}
+
+        {/*    <WarbondItem width={401} height={351} price={64} />*/}
+
+        {/*    <div className="grid gap-y-[17px] h-auto">*/}
+        {/*      <WarbondItem width={192} height={167} price={48} />*/}
+
+        {/*      <WarbondItem width={192} height={167} price={20} />*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+
         <div className="flex items-center gap-x-[17px] mx-auto w-[1000px] h-[540px]">
           <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[401px] h-full">
             <WarbondItem width={192} height={351} price={2} />
 
-            <WarbondItem width={192} height={351} price={15} />
+            <WarbondItem width={192} height={351} price={18} />
 
             <WarbondItem width={401} height={172} price={8} />
           </div>
@@ -38,18 +122,12 @@ const TheWarbondContent = () => {
         </div>
 
         <div className="flex items-center gap-x-[17px] mx-auto w-[1000px] h-[540px]">
-          <div className="flex gap-x-[17px] gap-y-[17px] w-[401px] h-full">
-            <div className="grid gap-y-[17px] w-[192px] h-full">
-              <WarbondItem width={192} height={172} price={15} />
+          <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[401px] h-full">
+            <WarbondItem width={401} height={172} price={15} />
 
-              <WarbondItem width={192} height={351} price={5} />
-            </div>
+            <WarbondItem width={192} height={351} price={5} />
 
-            <div className="grid gap-y-[17px] w-[192px] h-full">
-              <WarbondItem width={192} height={351} price={25} />
-
-              <WarbondItem width={192} height={172} price={16} />
-            </div>
+            <WarbondItem width={192} height={351} price={25} />
           </div>
 
           <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[610px] h-full">
@@ -58,7 +136,7 @@ const TheWarbondContent = () => {
             <div className="grid gap-y-[17px] h-auto">
               <WarbondItem width={192} height={167} price={35} />
 
-              <WarbondItem width={192} height={167} price={16} />
+              <WarbondItem width={192} height={167} price={20} />
             </div>
 
             <div className="flex gap-x-[17px] w-full">
@@ -73,7 +151,7 @@ const TheWarbondContent = () => {
           <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[401px] h-full">
             <WarbondItem width={192} height={351} price={16} />
 
-            <WarbondItem width={192} height={351} price={75} />
+            <WarbondItem width={192} height={351} price={60} />
 
             <WarbondItem width={401} height={172} price={32} />
           </div>
@@ -90,7 +168,7 @@ const TheWarbondContent = () => {
             <div className="grid gap-y-[17px] h-auto">
               <WarbondItem width={192} height={167} price={48} />
 
-              <WarbondItem width={192} height={167} price={20} />
+              <WarbondItem width={192} height={167} price={30} />
             </div>
           </div>
         </div>
