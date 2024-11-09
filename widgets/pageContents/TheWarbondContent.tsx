915f445a6a6919ca79c27d665fd96df7631aa088
Boris Karabut <boriskarabytwork@gmail.com>
2024-11-09 18:27:19 +0300

    chore: Полностью наполнил контентом второй варбонд

--- widgets/pageContents/TheWarbondContent.tsx
+++ widgets/pageContents/TheWarbondContent.tsx
@@ -26,14 +26,14 @@ const TheWarbondContent = () => {
               width={192}
               height={351}
               price={15}
-              image={"/static/warbonds/P4.png"}
+              image={"/static/warbonds/weapons/P4.webp"}
             />
 
             <WarbondItem
               width={401}
               height={172}
               price={8}
-              image={"/static/warbonds/SteeledVeteransEmote1.png"}
+              image={"/static/warbonds/emotes/SteeledVeteransEmote1.webp"}
             />
           </div>
 
@@ -50,7 +50,7 @@ const TheWarbondContent = () => {
                 width={401}
                 height={172}
                 price={20}
-                image={"/static/warbonds/AR23C.png"}
+                image={"/static/warbonds/weapons/AR23C.webp"}
               />
             </div>
 
@@ -58,7 +58,7 @@ const TheWarbondContent = () => {
               width={401}
               height={351}
               price={18}
-              image={"/static/warbonds/SA25Body.png"}
+              image={"/static/warbonds/armor/SA25Body.webp"}
             />
 
             <div className="grid gap-y-[17px] h-auto">
@@ -66,14 +66,14 @@ const TheWarbondContent = () => {
                 width={192}
                 height={167}
                 price={12}
-                image={"/static/warbonds/SA25Helmet.png"}
+                image={"/static/warbonds/armor/SA25Helmet.webp"}
               />
 
               <WarbondItem
                 width={192}
                 height={167}
                 price={8}
-                image={"/static/warbonds/SteeledVeteransCoat1.png"}
+                image={"/static/warbonds/coats/SteeledVeteransCoat1.webp"}
               />
             </div>
           </div>
@@ -86,7 +86,7 @@ const TheWarbondContent = () => {
                 width={192}
                 height={172}
                 price={15}
-                image={"/static/warbonds/SteeledVeteransEmote2.png"}
+                image={"/static/warbonds/emotes/SteeledVeteransEmote2.webp"}
               />
 
               <WarbondItem
@@ -102,14 +102,14 @@ const TheWarbondContent = () => {
                 width={192}
                 height={351}
                 price={25}
-                image={"/static/warbonds/G10.png"}
+                image={"/static/warbonds/weapons/G10.webp"}
               />
 
               <WarbondItem
                 width={192}
                 height={172}
                 price={16}
-                image={"/static/warbonds/SteeledVeteransCoat2.png"}
+                image={"/static/warbonds/coats/SteeledVeteransCoat2.webp"}
               />
             </div>
           </div>
@@ -119,7 +119,7 @@ const TheWarbondContent = () => {
               width={401}
               height={351}
               price={45}
-              image={"/static/warbonds/SA12Body.png"}
+              image={"/static/warbonds/armor/SA12Body.webp"}
             />
 
             <div className="grid gap-y-[17px] h-auto">
@@ -127,14 +127,14 @@ const TheWarbondContent = () => {
                 width={192}
                 height={167}
                 price={35}
-                image={"/static/warbonds/SA12Helmet.png"}
+                image={"/static/warbonds/armor/SA12Helmet.webp"}
               />
 
               <WarbondItem
                 width={192}
                 height={167}
                 price={16}
-                image={"/static/warbonds/SteeledVeteransCoat3.png"}
+                image={"/static/warbonds/coats/SteeledVeteransCoat3.webp"}
               />
             </div>
 
@@ -150,7 +150,7 @@ const TheWarbondContent = () => {
                 width={401}
                 height={172}
                 price={60}
-                image={"/static/warbonds/SG225IE.png"}
+                image={"/static/warbonds/weapons/SG225IE.webp"}
               />
             </div>
           </div>
@@ -176,7 +176,7 @@ const TheWarbondContent = () => {
               width={401}
               height={172}
               price={32}
-              image={"/static/warbonds/SteeledVeteransEmote3.png"}
+              image={"/static/warbonds/emotes/SteeledVeteransEmote3.webp"}
             />
           </div>
 
@@ -193,7 +193,7 @@ const TheWarbondContent = () => {
                 width={401}
                 height={172}
                 price={80}
-                image={"/static/warbonds/JAR5.png"}
+                image={"/static/warbonds/weapons/JAR5.webp"}
               />
             </div>
 
@@ -201,7 +201,7 @@ const TheWarbondContent = () => {
               width={401}
               height={351}
               price={64}
-              image={"/static/warbonds/SA32Body.png"}
+              image={"/static/warbonds/armor/SA32Body.webp"}
             />
 
             <div className="grid gap-y-[17px] h-auto">
@@ -209,14 +209,14 @@ const TheWarbondContent = () => {
                 width={192}
                 height={167}
                 price={48}
-                image={"/static/warbonds/SA32Helmet.png"}
+                image={"/static/warbonds/armor/SA32Helmet.webp"}
               />
 
               <WarbondItem
                 width={192}
                 height={167}
                 price={20}
-                image={"/static/warbonds/SteeledVeteransCoat4.png"}
+                image={"/static/warbonds/coats/SteeledVeteransCoat4.webp"}
               />
             </div>
           </div>
@@ -241,7 +241,12 @@ const TheWarbondContent = () => {
               image={"/static/warbonds/Booster.webp"}
             />
 
-            <WarbondItem width={401} height={172} price={8} />
+            <WarbondItem
+              width={401}
+              height={172}
+              price={8}
+              image={"/static/warbonds/emotes/CuttingEdgeEmote1.webp"}
+            />
           </div>
 
           <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[610px] h-full">
@@ -253,22 +258,47 @@ const TheWarbondContent = () => {
                 image={"/static/warbonds/SuperCredits.webp"}
               />
 
-              <WarbondItem width={401} height={172} price={20} />
+              <WarbondItem
+                width={401}
+                height={172}
+                price={20}
+                image={"/static/warbonds/weapons/LAS16.webp"}
+              />
             </div>
 
-            <WarbondItem width={401} height={351} price={18} />
+            <WarbondItem
+              width={401}
+              height={351}
+              price={18}
+              image={"/static/warbonds/armor/EX03Body.webp"}
+            />
 
             <div className="grid gap-y-[17px] h-auto">
-              <WarbondItem width={192} height={167} price={12} />
+              <WarbondItem
+                width={192}
+                height={167}
+                price={12}
+                image={"/static/warbonds/armor/EX03Helmet.webp"}
+              />
 
-              <WarbondItem width={192} height={167} price={8} />
+              <WarbondItem
+                width={192}
+                height={167}
+                price={8}
+                image={"/static/warbonds/coats/CuttingEdgeCoat1.webp"}
+              />
             </div>
           </div>
         </div>
 
         <div className="flex items-center gap-x-[17px] mx-auto w-[1000px] h-[540px]">
           <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[401px] h-full">
-            <WarbondItem width={401} height={172} price={15} />
+            <WarbondItem
+              width={401}
+              height={172}
+              price={15}
+              image={"/static/warbonds/emotes/CuttingEdgeEmote2.webp"}
+            />
 
             <WarbondItem
               width={192}
@@ -277,16 +307,36 @@ const TheWarbondContent = () => {
               image={"/static/PlayerCards/CuttingEdgeCard2.webp"}
             />
 
-            <WarbondItem width={192} height={351} price={25} />
+            <WarbondItem
+              width={192}
+              height={351}
+              price={25}
+              image={"/static/warbonds/weapons/G23.webp"}
+            />
           </div>
 
           <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[610px] h-full">
-            <WarbondItem width={401} height={351} price={45} />
+            <WarbondItem
+              width={401}
+              height={351}
+              price={45}
+              image={"/static/warbonds/armor/EX13Body.webp"}
+            />
 
             <div className="grid gap-y-[17px] h-auto">
-              <WarbondItem width={192} height={167} price={35} />
+              <WarbondItem
+                width={192}
+                height={167}
+                price={35}
+                image={"/static/warbonds/armor/EX13Helmet.webp"}
+              />
 
-              <WarbondItem width={192} height={167} price={20} />
+              <WarbondItem
+                width={192}
+                height={167}
+                price={20}
+                image={"/static/warbonds/coats/CuttingEdgeCoat2.webp"}
+              />
             </div>
 
             <div className="flex gap-x-[17px] w-full">
@@ -297,7 +347,12 @@ const TheWarbondContent = () => {
                 image={"/static/warbonds/SuperCredits.webp"}
               />
 
-              <WarbondItem width={401} height={172} price={60} />
+              <WarbondItem
+                width={401}
+                height={172}
+                price={60}
+                image={"/static/warbonds/weapons/SG8P.webp"}
+              />
             </div>
           </div>
         </div>
@@ -311,9 +366,19 @@ const TheWarbondContent = () => {
               image={"/static/PlayerCards/CuttingEdgeCard3.webp"}
             />
 
-            <WarbondItem width={192} height={351} price={60} />
+            <WarbondItem
+              width={192}
+              height={351}
+              price={60}
+              image={"/static/warbonds/weapons/LAS7.webp"}
+            />
 
-            <WarbondItem width={401} height={172} price={32} />
+            <WarbondItem
+              width={401}
+              height={172}
+              price={32}
+              image={"/static/warbonds/emotes/CuttingEdgeEmote3.webp"}
+            />
           </div>
 
           <div className="flex flex-wrap gap-x-[17px] gap-y-[17px] w-[610px] h-full">
@@ -325,15 +390,35 @@ const TheWarbondContent = () => {
                 image={"/static/warbonds/SuperCredits.webp"}
               />
 
-              <WarbondItem width={401} height={172} price={80} />
+              <WarbondItem
+                width={401}
+                height={172}
+                price={80}
+                image={"/static/warbonds/weapons/ARC12.webp"}
+              />
             </div>
 
-            <WarbondItem width={401} height={351} price={64} />
+            <WarbondItem
+              width={401}
+              height={351}
+              price={64}
+              image={"/static/warbonds/armor/EX00Body.webp"}
+            />
 
             <div className="grid gap-y-[17px] h-auto">
-              <WarbondItem width={192} height={167} price={48} />
+              <WarbondItem
+                width={192}
+                height={167}
+                price={48}
+                image={"/static/warbonds/armor/EX00Helmet.webp"}
+              />
 
-              <WarbondItem width={192} height={167} price={30} />
+              <WarbondItem
+                width={192}
+                height={167}
+                price={30}
+                image={"/static/warbonds/coats/CuttingEdgeCoat3.webp"}
+              />
             </div>
           </div>
         </div>
