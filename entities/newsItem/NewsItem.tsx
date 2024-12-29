92215605459a3908bf30d7565a3eaa405ad3ad6e
Boris Karabut <boriskarabytwork@gmail.com>
2024-12-29 16:42:22 +0300

    сохранение изменений

--- entities/newsItem/NewsItem.tsx
+++ entities/newsItem/NewsItem.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
+import Link from "next/link";
+
 import "./newsItem.css";
 
 interface NewsItemProps {
+  id: number;
   title: string;
   createdAt: string;
   cover: string;
 }
 
-const NewsItem: React.FC<NewsItemProps> = ({ title, createdAt, cover }) => {
+const NewsItem: React.FC<NewsItemProps> = ({ id, title, createdAt, cover }) => {
   const formatDate = () => {
     const date = new Date(createdAt);
 
@@ -36,15 +39,17 @@ const NewsItem: React.FC<NewsItemProps> = ({ title, createdAt, cover }) => {
     return `${day} ${formattedMonth} ${year}`;
   };
   return (
-    <div className="newsItem">
-      <span className="newsItem-head">
-        <h4 className="newsItem-head-title">{title}</h4>
+    <Link href={`/news/${id}`}>
+      <div className="newsItem">
+        <span className="newsItem-head">
+          <h4 className="newsItem-head-title">{title}</h4>
 
-        <p className="newsItem-head-date">{formatDate()}</p>
-      </span>
+          <p className="newsItem-head-date">{formatDate()}</p>
+        </span>
 
-      <img src={cover} alt={title} className="newsItem-cover" />
-    </div>
+        <img src={cover} alt={title} className="newsItem-cover" />
+      </div>
+    </Link>
   );
 };
 
