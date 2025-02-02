de1ff9a2566b7d0e6396cbef09d627142a8322a2
boris <b.karabyt@gmail.com>
2025-02-02 15:48:53 +0300

    feat: Добавил hover эффект для карточки брони

--- entities/armorKitsPage/armorKit/ArmorKit.tsx
+++ entities/armorKitsPage/armorKit/ArmorKit.tsx
@@ -3,12 +3,13 @@ import React from "react";
 
 import { useTranslations } from "next-intl";
 
-import { getIntlArray } from "@/utils/generalFunctions";
+import { getIntlArray, toSlug } from "@/utils/generalFunctions";
 
 import { ArmorBonus } from "@/data/armor/bonuses";
 
 import { armorStore } from "@/store/ArmorKitsStore";
-import { slidersStore } from "@/store/SlidersStore";
+
+import Link from "next/link";
 
 import Image from "next/image";
 
@@ -20,44 +21,51 @@ interface ArmorKitProps {
   id: number;
   imagePath: string;
   type: string;
+  devName: string;
   bonus: ArmorBonus;
 }
 
-const ArmorKit: React.FC<ArmorKitProps> = ({ id, imagePath, type, bonus }) => {
+const ArmorKit: React.FC<ArmorKitProps> = ({
+  id,
+  imagePath,
+  type,
+  devName,
+  bonus,
+}) => {
   const t = useTranslations("armor");
 
   const handleCurrentArmorInfoChange = () => {
-    slidersStore.handleCurrentSlideChange(id);
-
     armorStore.changeCurrentArmorInfo(id, type);
   };
   return (
-    <div className="armorKit">
-      <div
-        onClick={() => handleCurrentArmorInfoChange()}
-        className="armorKitCard"
-      >
-        <Tooltip
-          placement="top"
-          title={bonus.name + "\n" + bonus.description}
-          arrow={true}
-          overlayStyle={{ whiteSpace: "pre-line" }}
-          overlayInnerStyle={{ textAlign: "center" }}
+    <Link href={`/equipment/armor/${toSlug(devName)}`}>
+      <div className="armorKit">
+        <div
+          onClick={() => handleCurrentArmorInfoChange()}
+          className="armorKitCard"
         >
-          <Image
-            src={bonus?.icon}
-            alt={bonus?.name}
-            width={40}
-            height={40}
-            className="armorKitCard-bonusIcon"
-          />
-        </Tooltip>
+          <Tooltip
+            placement="top"
+            title={bonus.name + "\n" + bonus.description}
+            arrow={true}
+            overlayStyle={{ whiteSpace: "pre-line" }}
+            overlayInnerStyle={{ textAlign: "center" }}
+          >
+            <Image
+              src={bonus?.icon}
+              alt={bonus?.name}
+              width={40}
+              height={40}
+              className="armorKitCard-bonusIcon"
+            />
+          </Tooltip>
 
-        <img src={imagePath} alt="" className="armorKitCard-image" />
+          <img src={imagePath} alt="" className="armorKitCard-image" />
+        </div>
+
+        <p className="armorKit-text">{getIntlArray(t("names"))[id - 1]}</p>
       </div>
-
-      <p className="armorKit-text">{getIntlArray(t("names"))[id - 1]}</p>
-    </div>
+    </Link>
   );
 };
 
