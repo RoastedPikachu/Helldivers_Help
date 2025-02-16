57af2b73d5e87037f09a6641765c91f3f9d9d518
boris <b.karabyt@gmail.com>
2025-02-16 19:28:13 +0300

    feat: Перевел страницу 404

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,26 +1,29 @@
+"use client";
 import React from "react";
 
 import Link from "next/link";
 
 import "./NotFoundPage.css";
+import { useTranslations } from "next-intl";
 
 const NotFound = () => {
+  const t = useTranslations("NotFoundPage");
+
   return (
     <main className="flex justify-center items-center mt-0 w-screen h-screen">
       <div className="contentBlock">
-        <h1 className="contentBlock_Title">Упс!</h1>
+        <h1 className="contentBlock_Title">{t("pageTitle")}</h1>
 
         <div className="contentBlock_Bottom">
           <h2 className="contentBlock_Bottom_Title">
-            Ошибка <b className="contentBlock_Bottom_Title_Bold">404</b>
+            {t("errorText")}{" "}
+            <b className="contentBlock_Bottom_Title_Bold">404</b>
           </h2>
 
-          <p className="contentBlock_Bottom_Text">
-            Похоже что страница, которую вы искали, не найдена.
-          </p>
+          <p className="contentBlock_Bottom_Text">{t("pageDescription")}</p>
 
           <Link href="/" className="contentBlock_Bottom_LinkButton">
-            Вернуться на главную
+            {t("linkText")}
           </Link>
         </div>
       </div>
