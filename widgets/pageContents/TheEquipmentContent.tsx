5ac7831d3df62095d71742bb351405e9db9a88bd
boris <b.karabyt@gmail.com>
2025-02-18 16:40:26 +0300

    feat: адаптировал страницу снаряжения под мобильные устройства

--- widgets/pageContents/TheEquipmentContent.tsx
+++ widgets/pageContents/TheEquipmentContent.tsx
@@ -19,45 +19,45 @@ const TheEquipmentContent = () => {
 
       <PageDescription description={t("pageDescription")} />
 
-      <section className="relative grid gap-x-[20px] grid-cols-3 mt-[30px]">
+      <section className="relative grid gap-x-[20px] gap-y-[20px] grid-cols-3 mlarge:grid-cols-1 mt-[30px]">
         <Link href={"/equipment/armor"}>
           <div className="relative flex justify-between items-center py-[10px] px-[20px] w-auto h-auto bg-[url('/static/EquipmentBackground.png')] bg-cover border-2 border-theme rounded-[10px] duration-300 ease-in-out hover:brightness-[1.15]">
-            <p className="mt-[-5px] text-white text-[2rem] font-primary font-bold">
+            <p className="mt-[-5px] text-white text-[2rem] mlarge:text-[1.75rem] mmedium:text-[1.5rem] font-primary font-bold">
               {getIntlArray(t("links"))[0]}
             </p>
 
             <img
               src={"/static/ArmorIcon.svg"}
               alt=""
-              className="w-[52px] h-[52px]"
+              className="w-[52px] mmedium:w-[47px] msmall:w-[42px] h-[52px] mmedium:h-[47px] msmall:h-[42px]"
             />
           </div>
         </Link>
 
         <Link href={"/equipment/boosters"}>
           <div className="relative flex justify-between items-center py-[10px] px-[20px] w-auto h-auto bg-[url('/static/EquipmentBackground.png')] bg-cover border-2 border-theme rounded-[10px] duration-300 ease-in-out hover:brightness-[1.15]">
-            <p className="mt-[-5px] text-white text-[2rem] font-primary font-bold">
+            <p className="mt-[-5px] text-white text-[2rem] mlarge:text-[1.75rem] mmedium:text-[1.5rem] font-primary font-bold">
               {getIntlArray(t("links"))[1]}
             </p>
 
             <img
               src={"/static/BoosterIcon.png"}
               alt=""
-              className="w-[52px] h-[52px]"
+              className="w-[52px] mmedium:w-[47px] msmall:w-[42px] h-[52px] mmedium:h-[47px] msmall:h-[42px]"
             />
           </div>
         </Link>
 
         <Link href={"/equipment/capes"}>
           <div className="relative flex justify-between items-center py-[10px] px-[20px] w-auto h-auto bg-[url('/static/EquipmentBackground.png')] bg-cover border-2 border-theme rounded-[10px] duration-300 ease-in-out hover:brightness-[1.15]">
-            <p className="mt-[-5px] text-white text-[2rem] font-primary font-bold">
+            <p className="mt-[-5px] text-white text-[2rem] mlarge:text-[1.75rem] mmedium:text-[1.5rem] font-primary font-bold">
               {getIntlArray(t("links"))[2]}
             </p>
 
             <img
               src={"/static/CapeIcon.svg"}
               alt=""
-              className="w-[52px] h-[52px]"
+              className="w-[52px] mmedium:w-[47px] msmall:w-[42px] h-[52px] mmedium:h-[47px] msmall:h-[42px]"
             />
           </div>
         </Link>
