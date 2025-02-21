e9e2d3c08863a661cca3aa1d493073a22ed8e897
boris <b.karabyt@gmail.com>
2025-02-21 11:00:23 +0300

    fix: починил адаптив у шапки для мобильных устройств

--- widgets/mobileHeaderContent/MobileHeaderContent.tsx
+++ widgets/mobileHeaderContent/MobileHeaderContent.tsx
@@ -8,9 +8,11 @@ import Link from "next/link";
 import { pages } from "@/data/pages/pages";
 
 import "./MobileHeaderContent.css";
+import { getIntlArray } from "@/utils/generalFunctions";
 
 const MobileHeaderContent = () => {
-  const t = useTranslations("Pages");
+  const t = useTranslations("Header");
+  const t1 = useTranslations("Pages");
 
   const [
     isAdditionalHeaderInfoOpened,
@@ -29,13 +31,13 @@ const MobileHeaderContent = () => {
         >
           <div className="relative w-[35px] mmedium:w-[32.5px] msmall:w-[30px] h-[28px] mmedium:h-[25px] msmall:h-[23px] duration-[400ms] ease-in-out">
             <span
-              className={`absolute left-0 w-full h-[5px] bg-[#2cc388] rounded-[10px] duration-[250ms] ease-in-out ${isAdditionalHeaderInfoOpened ? "top-[11px] -rotate-45" : "top-0"}`}
+              className={`absolute left-0 w-full h-[5px] bg-theme rounded-[10px] duration-[250ms] ease-in-out ${isAdditionalHeaderInfoOpened ? "top-[11px] -rotate-45" : "top-0"}`}
             ></span>
             <span
-              className={`absolute top-[11px] left-0 w-full h-[5px] bg-[#2cc388] rounded-[10px] duration-[250ms] ease-in-out ${isAdditionalHeaderInfoOpened ? "w-0 opacity-0" : ""}`}
+              className={`absolute top-[11px] left-0 w-full h-[5px] bg-theme rounded-[10px] duration-[250ms] ease-in-out ${isAdditionalHeaderInfoOpened ? "w-0 opacity-0" : ""}`}
             ></span>
             <span
-              className={`absolute left-0 w-full h-[5px] bg-[#2cc388] rounded-[10px] duration-[250ms] ease-in-out ${isAdditionalHeaderInfoOpened ? "top-[11px] rotate-45" : "top-[22px]"}`}
+              className={`absolute left-0 w-full h-[5px] bg-theme rounded-[10px] duration-[250ms] ease-in-out ${isAdditionalHeaderInfoOpened ? "top-[11px] rotate-45" : "top-[22px]"}`}
             ></span>
           </div>
         </button>
@@ -46,37 +48,35 @@ const MobileHeaderContent = () => {
       </div>
 
       <div
-        className={`fixed w-full bg-[#01212f] duration-[400ms] ease-in-out overflow-y-scroll overscroll-contain z-40 ${window.location.href.includes("/stratagemTraining") ? "top-[65px] pb-[50px] h-[calc(100vh-50px)]" : "top-[115px] msmall:top-[110px] pb-[50px] h-[calc(100vh-115px)] msmall:h-[calc(100vh-110px)]"} ${isAdditionalHeaderInfoOpened ? "left-0" : "left-[-100vw]"}`}
+        className={`fixed w-full bg-primary-bg duration-[400ms] ease-in-out overflow-y-scroll overscroll-contain z-40 ${window.location.href.includes("/stratagemtraining") ? "top-[65px] pb-[50px] h-[calc(100vh-50px)]" : "top-[115px] msmall:top-[110px] pb-[50px] h-[calc(100vh-115px)] msmall:h-[calc(100vh-110px)]"} ${isAdditionalHeaderInfoOpened ? "left-0" : "left-[-100vw]"}`}
       >
         <nav className="mt-[30px] px-[calc(2.5%+10px)]">
-          <div className="flex justify-between items-center w-full">
+          <div className="relative grid gap-y-[10px] w-full h-auto">
             <Link
-              href="/code"
-              className="headerNavBlock_Link py-[10px] msmall:py-[5px] px-[15px] msmall:px-[10px] border-2 border-[#2cc388] rounded-[10px]"
+              href="/stratagemtraining"
+              className="headerNavBlock_Link py-[10px] msmall:py-[5px] px-[15px] msmall:px-[10px] bg-[#1e1e1e] border-2 border-theme rounded-[10px]"
             >
-              КОДЕКС
+              {getIntlArray(t("links"))[2]}
             </Link>
 
-            <img
-              src="/static/GeneralIcons/SkullIcon.svg"
-              alt=""
-              className="headerLogoLink_Icon"
-            />
+            {/*<img*/}
+            {/*  src="/static/GeneralIcons/SkullIcon.svg"*/}
+            {/*  alt=""*/}
+            {/*  className="headerLogoLink_Icon"*/}
+            {/*/>*/}
 
             <Link
               href="/war"
-              className="headerNavBlock_Link py-[10px] msmall:py-[5px] px-[15px] msmall:px-[10px] border-2 border-[#2cc388] rounded-[10px]"
+              className="headerNavBlock_Link py-[10px] msmall:py-[5px] px-[15px] msmall:px-[10px] bg-[#1e1e1e] border-2 border-theme rounded-[10px]"
             >
-              ВОЙНА
+              {getIntlArray(t("links"))[1]}
             </Link>
-          </div>
 
-          <div className="flex justify-center items-center mt-[20px] w-full">
             <Link
-              href="/stratagemTraining"
-              className="headerNavBlock_Link py-[10px] msmall:py-[5px] px-[15px] msmall:px-[10px] border-2 border-[#2cc388] rounded-[10px]"
+              href="/news"
+              className="headerNavBlock_Link py-[10px] msmall:py-[5px] px-[15px] msmall:px-[10px] bg-[#1e1e1e] border-2 border-theme rounded-[10px]"
             >
-              ТРЕНИРОВКА СТРАТАГЕМ
+              {getIntlArray(t("links"))[0]}
             </Link>
           </div>
 
@@ -84,10 +84,10 @@ const MobileHeaderContent = () => {
             {pages.map((page) => (
               <Link
                 key={page.id}
-                href={`${page.link}`}
+                href={page.link}
                 className="block text-[#ffffff] text-[1.25rem] msmall:text-[1.125rem] text-left font-['Exo2'] font-bold outline-none"
               >
-                {t(`${page.id}Link`).toUpperCase()}
+                {getIntlArray(t1("links"))[page.id - 1].toUpperCase()}
               </Link>
             ))}
           </div>
