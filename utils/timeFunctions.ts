045da6710e2d02c9433295cf280e28e0c0518152
boris <b.karabyt@gmail.com>
2025-03-08 22:03:14 +0300

    chore: Added required functions for campaigns

--- utils/timeFunctions.ts
+++ utils/timeFunctions.ts
@@ -6,9 +6,36 @@ export function timeFromNow(unixTimestamp: any) {
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds % 60);
 
-  return (
-    hours.toString().padStart(2, "0") +
-    ":" +
-    minutes.toString().padStart(2, "0")
-  );
+  if (hours === 0) {
+    return (
+      minutes.toString().padStart(2, "0") +
+      "мин " +
+      seconds.toString().padStart(2, "0") +
+      "c"
+    );
+  }
+
+  if (hours > 10) {
+    return (
+      hours.toString().padStart(2, "0") +
+      "ч " +
+      minutes.toString().padStart(2, "0") +
+      "мин"
+    );
+  }
+
+  return hours.toString() + "ч " + minutes.toString() + "мин";
 }
+
+export const calculateTimePercentage = (
+  unixTimestamp: any,
+  endUnixTimestamp: any,
+  startUnixTimestamp: any,
+) => {
+  const currentTime = Math.floor(Date.now() / 1000);
+  const secondsLeft = unixTimestamp - currentTime;
+
+  const totalSeconds = endUnixTimestamp - startUnixTimestamp;
+
+  return 99 - (secondsLeft / totalSeconds) * 100;
+};
